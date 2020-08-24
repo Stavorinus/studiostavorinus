a245e4ed01a17e07188043deb69332bfc4bf9ab2
Studio Stavorinus <info@stavorinus.com>
2020-08-24 20:06:52 +0200

    UPD: Layout base

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Studio Stavorinus`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    author: `Niels Stavorinus`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
